73dde00888ca9c9f1e0a9f8ea7baf7aa570714fa
ofekdemri <angrybeats@gmail.com>
2019-05-26 17:04:06 +0300

    email fix

--- initvars.bat
+++ initvars.bat
@@ -1,8 +1,8 @@
 set FLASK_APP=clasifyIT.py
 set SQLALCHEMY_DATABASE_URI=sqlite:///db.sqlite
 set SECRET_KEY=5791628bb0b13ce0c676dfdsdsde280ba245gh7868689
-set EMAIL_USER=clasifyproject@gmail.com
-set EMAIL_PASSWORD=ClasifyModel!
+set EMAIL_USER=uselesscode12@gmail.com
+set EMAIL_PASSWORD=adelitas1
 set FLASK_ENV=development
 FLASK run
 
